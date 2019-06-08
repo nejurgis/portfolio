7317c124fe63768ab055b25019da96ba4b9c43a2
quietQuiet <j.lietunovas@gmail.com>
2019-06-08 11:31:26 +0200

    added css corrections

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,13 @@ import React from "react"
 import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
 
-const Wrapper = styled("div")`
-  border: 2px solid green;
-  padding: 10px;
-`
+// const Wrapper = styled("div")`
+//   border: 2px solid green;
+//   padding: 10px;
+// `
 
 export default ({ children }) => (
-  <Wrapper>
+  <>
     <Global
       styles={css`
         div {
@@ -22,5 +22,5 @@ export default ({ children }) => (
       `}
     />
     {children}
-  </Wrapper>
+  </>
 )
